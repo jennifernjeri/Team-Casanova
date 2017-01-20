cc37bd7a1b424875fe0598c0ccffcc8b11c77446
Maxwell-icharia <maxwellicharia@gmail.com>
2017-01-20 16:01:25 +0300

    Modify add_skill functions

--- add_skill.py
+++ add_skill.py
@@ -1,20 +1,18 @@
-# from tabulate import tabulate
 class Skills(object):
+
 	def __init__(self):
 		self.skill = {}
 
-	def get_skill_id(self):
-	    id = int(input("Enter id of skill:"))
-	    return id
-
-	def get_skill_name(self):
-	    name = input("Enter name of skill:")
-	    return name
-
 	def add_skill(self):
-	    self.skill.update({'id': get_skill_id(), 'value': get_skill_name()})
+		id = int(input("Enter id of skill:"))
+		name = input("Enter name of skill:")
+		self.skill.update({'id': id, 'value': name})
 
 	def view_skill(self):
-		return self.skill
+		print "Your skills are:" 
+		print self.skill
 
+skills = Skills()
+skills.add_skill()
+skills.view_skill()
 
